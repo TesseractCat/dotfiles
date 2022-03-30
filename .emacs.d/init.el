43a404d1b94d1db2b1060a153d730b5283067bb4
Tesseract Cat <tesseractcats@gmail.com>
2022-03-29 20:41:05 -0400

    Convert to `use-package` macro

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,19 +1,7 @@
 (require 'package)
 
 ;; Package list
-(setq package-list '(evil ;; evil
-                     evil-leader
-                     evil-collection
-                     evil-mc
-                     counsel ;; ivy
-                     lsp-mode ;; lsp
-                     lsp-ui
-                     company
-                     rust-mode ;; languages
-                     csharp-mode
-                     magit ;; misc
-                     mood-line
-                     ))
+(setq package-list '(use-package))
 
 ;; Add Melpa as the default Emacs Package repository
 (add-to-list 'package-archives
@@ -31,58 +19,166 @@
   (unless (package-installed-p package)
     (package-install package)))
 
+;; Display loading time/order for packages
+;; (setq use-package-verbose t)
+
 ;; Evil mode settings
-(setq evil-want-integration t
-      evil-want-keybinding nil)
 
-(setq evil-split-window-below t
-      evil-vsplit-window-right t
-      evil-want-C-u-scroll t
-      evil-echo-state nil)
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-integration t
+        evil-want-keybinding nil)
+  (setq evil-split-window-below t
+        evil-vsplit-window-right t
+        evil-want-C-u-scroll t
+        evil-echo-state nil)
+  :config
+  (evil-mode t))
 
-(require 'evil-leader)
-(global-evil-leader-mode)
-(evil-leader/set-leader "<SPC>")
-(evil-leader/set-key
-  "x" 'counsel-M-x
-  "b" 'counsel-bookmark
-  "g" 'magit-status
-  "h" 'help-for-help
-  "e" 'eval-defun
-  "ff" 'find-function
-  "q" 'evil-mc-undo-all-cursors
-  "s" 'evil-mc-make-cursors-at-search
-  "w" 'save-buffer)
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (delete 'evil-mc evil-collection-mode-list)
+  (evil-collection-init))
 
-(require 'evil)
-(evil-mode t)
+(use-package evil-leader
+  :after evil-collection
+  :ensure t
+  :config
+  (global-evil-leader-mode)
+  (evil-leader/set-leader "<SPC>")
+  (evil-leader/set-key
+    "x" 'execute-extended-command
+    "b" 'bookmark-jump
+    "g" 'magit-status
+    "h" 'help-for-help
+    "e" 'eval-defun
+    "ff" 'find-function
+    "q" 'evil-mc-undo-all-cursors
+    "s" 'evil-mc-make-cursors-at-search
+    "w" 'save-buffer))
 
-(require 'evil-collection)
-(delete 'evil-mc evil-collection-mode-list)
-(evil-collection-init)
+(use-package evil-mc
+  :after evil-leader
+  :ensure t
+  :init
+  (setq evil-mc-enable-bar-cursor nil
+        evil-mc-undo-cursors-on-keyboard-quit t)
+  :config
+  (keymap-unset evil-mc-key-map "<normal-state> M-n")
+  (keymap-unset evil-mc-key-map "<normal-state> M-p")
+  (global-evil-mc-mode t)
 
-(require 'evil-mc)
-(global-evil-mc-mode t)
-(setq evil-mc-enable-bar-cursor nil)
-(setq evil-mc-undo-cursors-on-keyboard-quit t)
-(defun evil-mc-make-cursors-at-search ()
-  (interactive)
-  (let ((search (car evil-search-forward-history)))
-    (setq evil-mc-pattern (cons (evil-ex-make-search-pattern search) nil))
-    (evil-mc-make-cursors-for-all)
-    (let ((goto (or (evil-mc-find-next-cursor) (evil-mc-find-prev-cursor))))
-      (evil-mc-goto-cursor goto nil)
-      )))
+  (defun evil-mc-make-cursors-at-search ()
+    (interactive)
+    (let ((search (car evil-search-forward-history)))
+      (setq evil-mc-pattern (cons (evil-ex-make-search-pattern search) nil))
+      (evil-mc-make-cursors-for-all)
+      (let ((goto (or (evil-mc-find-next-cursor) (evil-mc-find-prev-cursor))))
+        (evil-mc-goto-cursor goto nil)
+        ))))
+
+(evil-global-set-key 'visual (kbd "gc") 'comment-dwim)
 
 ;; LSP/Language settings
 
-(evil-define-key 'normal 'global "gd" 'lsp-find-definition)
+(use-package lsp-mode
+  :ensure t
+  :init
+  (evil-define-key 'normal 'global "gd" 'lsp-find-definition)
+  (setq lsp-headerline-breadcrumb-enable nil)
+  (setq sgml-basic-offset 4)
+  :commands lsp)
+(use-package csharp-mode
+  :after lsp-mode
+  :ensure t)
+(use-package rust-mode
+  :after lsp-mode
+  :ensure t)
 
-(setq company-minimum-prefix-length 1
-      company-idle-delay 0.0) ;; default is 0.2
-(setq lsp-headerline-breadcrumb-enable nil)
+(use-package company
+  :ensure t
+  :init
+  (setq company-minimum-prefix-length 1
+        company-idle-delay 0.0) ;; default is 0.2
+  (setq company-format-margin-function 'company-text-icons-margin
+        company-text-icons-add-background t)
+  (setq company-global-modes '(not markdown-mode))
+  :config
+  (keymap-unset company-active-map "C-w")
+  (keymap-set company-active-map "TAB" 'company-complete-selection)
+  (keymap-set company-active-map "<tab>" 'company-complete-selection)
+  (global-company-mode t))
 
-(setq sgml-basic-offset 4)
+;; Markdown settings
+
+(use-package visual-fill-column
+  :ensure t)
+(use-package adaptive-wrap
+  :ensure t)
+(use-package markdown-mode
+  :defer t
+  :ensure t
+  :init
+  (setq markdown-enable-math t
+        markdown-enable-wiki-links t
+        markdown-wiki-link-alias-first nil
+        markdown-wiki-link-search-subdirectories t
+        markdown-link-space-sub-char " "
+        markdown-header-scaling t
+        markdown-mouse-follow-link t)
+  (add-hook 'markdown-mode-hook (lambda () ;; On markdown-mode:
+                                  (setq mode-line-format nil) ;; Hide mode line
+                                  (visual-line-mode t) ;; Visual wrap at column 80
+                                  (visual-fill-column-mode t)
+                                  (adaptive-wrap-prefix-mode t)))
+  :config
+  (keymap-set markdown-mode-map "M-n" (lambda ()
+                                        (interactive)
+                                        (call-interactively
+                                         'markdown-next-visible-heading)
+                                        (call-interactively
+                                         'evil-scroll-line-to-top)))
+  (keymap-set markdown-mode-map "M-p" (lambda ()
+                                        (interactive)
+                                        (call-interactively
+                                         'markdown-previous-visible-heading)
+                                        (call-interactively
+                                         'evil-scroll-line-to-top)))
+  (keymap-set markdown-mode-map "M-N" 'markdown-next-link)
+  (keymap-set markdown-mode-map "M-P" 'markdown-previous-link))
+
+(keymap-unset evil-motion-state-map "C-o")
+(keymap-global-set "C-o" 'find-file)
+
+;; Hide math-preview on hover
+(use-package math-preview
+  :ensure t
+  :config
+  (defvar-local math-preview-hover--prev-range nil)
+  (defun math-preview-hover--enter ()
+    (let ((overlay-here (car (overlays-at (point)))))
+      (when overlay-here
+        (when (string-equal (overlay-get overlay-here 'category) 'math-preview)
+          (setq math-preview-hover--prev-range
+                (cons (overlay-start overlay-here) (overlay-end overlay-here)))
+          (math-preview-clear-at-point)))))
+  (defun math-preview-hover--exit ()
+    (when math-preview-hover--prev-range
+      ;; Increment range on keypress
+      (when (eq last-command 'self-insert-command)
+        (setf (cdr math-preview-hover--prev-range) (1+ (cdr math-preview-hover--prev-range))))
+      ;; Exit current overlay range
+      (when (not (and (>= (point) (car math-preview-hover--prev-range)) (<= (point) (cdr math-preview-hover--prev-range))))
+        (save-excursion
+          (goto-char (car math-preview-hover--prev-range))
+          (math-preview-at-point))
+        (setq math-preview-hover--prev-range nil)
+    )))
+  (add-hook 'post-command-hook 'math-preview-hover--exit 1)
+  (add-hook 'post-command-hook 'math-preview-hover--enter 2))
 
 ;; Misc settings
 
@@ -101,15 +197,34 @@
       scroll-conservatively 10000
       scroll-preserve-screen-position 1)
 
+(setq split-width-threshold 1) ;; Always vsplit on split
+
 (set-frame-parameter (selected-frame) 'internal-border-width 40) ;; Padding
 
-(mood-line-mode)
+(use-package selectrum
+  :ensure t
+  :config
+  (selectrum-mode t))
+(use-package marginalia
+  :ensure t
+  :config
+  (marginalia-mode t))
+(use-package mood-line
+  :ensure t
+  :config
+  (mood-line-mode t))
+
+(use-package magit
+  :defer t
+  :ensure t)
 
 ;; Save window position/dimensions
 
 (setq desktop-path (list user-emacs-directory)) ;; Save in .emacs.d
 (setq desktop-buffers-not-to-save ".*"
       desktop-files-not-to-save ".*") ;; Don't save buffers
+(add-hook 'desktop-after-read-hook (lambda () ;; Don't save tabs
+                                     (tab-bar-close-other-tabs)))
 (setq desktop-save t) ;; Always save
 (desktop-save-mode t)
 
@@ -120,12 +235,13 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(minimal-black))
  '(custom-safe-themes
-   '("3e335d794ed3030fefd0dbd7ff2d3555e29481fe4bbb0106ea11c660d6001767" "4780d7ce6e5491e2c1190082f7fe0f812707fc77455616ab6f8b38e796cbffa9" "cc0dbb53a10215b696d391a90de635ba1699072745bf653b53774706999208e3" "33ea268218b70aa106ba51a85fe976bfae9cf6931b18ceaf57159c558bbcd1e6" default))
+   '("184b3a18e5d1ef9c8885ceb9402c3f67d656616d76bf91f709d1676407c7cf1d" "801a567c87755fe65d0484cb2bded31a4c5bb24fd1fe0ed11e6c02254017acb2" "dbade2e946597b9cda3e61978b5fcc14fa3afa2d3c4391d477bdaeff8f5638c5" "3e335d794ed3030fefd0dbd7ff2d3555e29481fe4bbb0106ea11c660d6001767" "4780d7ce6e5491e2c1190082f7fe0f812707fc77455616ab6f8b38e796cbffa9" "cc0dbb53a10215b696d391a90de635ba1699072745bf653b53774706999208e3" "33ea268218b70aa106ba51a85fe976bfae9cf6931b18ceaf57159c558bbcd1e6" default))
  '(display-line-numbers-type 'relative)
  '(evil-start-of-line t)
  '(evil-undo-system 'undo-redo)
+ '(math-preview-scale 0.8)
  '(package-selected-packages
-   '(mood-line minimal-theme csharp-mode evil-mc-extras lsp-ui lsp-mode evil-leader evil))
+   '(tao-theme use-package adaptive-wrap visual-fill-column math-preview mood-line minimal-theme csharp-mode evil-mc-extras lsp-ui lsp-mode evil-leader evil))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -134,4 +250,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Terminus" :foundry "raster" :slant normal :weight regular :height 151 :width normal))))
- '(fringe ((t (:background nil)))))
+ '(fringe ((t (:background nil))))
+ '(markdown-italic-face ((t (:inherit italic :foreground "light coral"))))
+ '(variable-pitch ((t (:foundry "outline" :family "Roman")))))
